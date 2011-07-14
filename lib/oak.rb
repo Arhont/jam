cf81423666201af06317041839f67cb2a0a7799b
Edward Tsech <edtsech@gmail.com>
2011-07-15 01:38:24 +0700

    Added test for only

--- lib/oak.rb
+++ lib/oak.rb
@@ -12,13 +12,27 @@ module Oak
       methods = options[:methods]
       root    = options[:root]
 
-      if methods
-        Hash[methods.map do |m|
-          [m, obj.send(m)]
-        end].merge(yield(obj))
-      else
-        yield(obj)
+      result = {}
+
+      if only
+        result.merge!(Hash[only.map do |m|
+          [m.to_s, obj.read_attribute(m)]
+        end])
+      elsif obj.respond_to? :attributes
+        result.merge!(obj.attributes)
       end
+
+      if methods
+        result.merge!(Hash[methods.map do |m|
+          [m, obj.send(m)]
+        end])
+      end
+
+      if block_given?
+        result.merge! yield(obj)
+      end
+
+      result
     end
   end
 end
--- test/oak_test.rb
+++ test/oak_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
-class OakTest < ActiveSupport::TestCase
-  test "truth" do
+class OakBuilderTest < ActiveSupport::TestCase
+  test "" do
     assert_kind_of Module, Oak
+  end
+
+  setup do
+    @post = Post.new :title => 'Hi!', :body => 'Sample text!'
+    @builder = Oak::Builder.new
+  end
+
+  test 'instance should respond to object' do
+    assert @builder.respond_to? :object
+  end
+
+  test 'basic test' do
+    post = @post
+
+    res = @builder.instance_eval do
+      object post
+    end
+
+    assert_equal ({"created_at"=>nil, "body"=>"Sample text!", "title"=>"Hi!", "updated_at"=>nil}), res
+
+
+    res = @builder.instance_eval do
+      object post, :only => [:title, :body]
+    end
+
+    assert_equal ({"body"=>"Sample text!", "title"=>"Hi!"}), res
   end
 end
